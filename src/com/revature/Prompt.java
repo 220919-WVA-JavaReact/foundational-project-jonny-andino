d593dda165fed38deeb4603f3dbab9915ba45896
bluephosphor <66392799+bluephosphor@users.noreply.github.com>
2022-10-01 23:45:08 -0400

    experimenting with ways to get input from cli

---
+++ src/com/revature/Prompt.java
@@ -0,0 +1,23 @@
+package com.revature;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class Prompt {
+
+    public static Scanner input = new Scanner(System.in);
+    public String ask(String question) {
+
+        System.out.println(question);
+        return input.nextLine();
+    }
+
+    public String getPass(String question){
+        Console cons;
+        if ((cons = System.console()) != null){
+            char[] pass = cons.readPassword(question);
+            return new String(pass);
+        }
+        return "console was null";
+    }
+}
