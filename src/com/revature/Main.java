f9eba796d9cd986f694a292b3f6271690e4bbf02
bluephosphor <66392799+bluephosphor@users.noreply.github.com>
2022-10-01 23:51:05 -0400

    added main

---
+++ src/com/revature/Main.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+public class Main {
+    public static void main(String[] args) {
+        Prompt userPrompt = new Prompt();
+        String uname = userPrompt.ask("Please input a username.");
+        System.out.println("Welcome back, " + uname + "!");
+        String pass = userPrompt.getPass("Please enter your Password.");
+        System.out.println(pass);
+    }
+}
